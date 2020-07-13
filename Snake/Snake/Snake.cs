4f7935c41d2f20de08ba927debf7e9c993d0f0ce
Ovsyannikov Sergey <Oza1999@mail.ru>
2020-07-13 12:12:25 +0300

    CourseTask10

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -8,16 +8,36 @@ namespace Snake
 {
     class Snake:Figure
     {
+        Direction _direction;
         public Snake(Point tail, int length, Direction direction)
         {
+            _direction = direction;
             line = new List<Point>();
             for(int j = 0; j< length; j++)
             {
                 Point p = new Point(tail);
-                p.Move(j,direction);
+                p.Move(j, _direction);
                 line.Add(p);
 
             }
+        }
+
+        internal void Move()
+        {
+            Point tail = line.First();
+            line.Remove(tail);
+            Point head = GetNextPoint();
+            line.Add(head);
+            tail.Clear();
+            head.Draw();
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = line.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, _direction);
+            return nextPoint;
         }
     }
 }
