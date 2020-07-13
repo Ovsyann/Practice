0333f7f680650bbb6e5928943d9175933f537b75
Ovsyannikov Sergey <Oza1999@mail.ru>
2020-07-13 11:16:11 +0300

    CourseTask7

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class HorizontalLine
+    {
+        List<Point> line;
+        public HorizontalLine(int xLeft,int xRight,int y,char sym)
+        {
+            line = new List<Point>();
+            for(int x = xLeft; x < xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                line.Add(p);
+            }
+        }
+        public void Draw()
+        {
+            foreach(Point i in line)
+            {
+                i.Draw();
+            }
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -18,23 +18,8 @@ namespace Snake
 
             point2.Draw();
 
-            List<int> list = new List<int>();
-            list.Add(0);
-            list.Add(1);
-            list.Add(2);
-
-            int x = list[0];
-            int y = list[1];
-            int z = list[2];
-
-            foreach(int i in list)
-            {
-                Console.WriteLine(i);
-            }
-            list.RemoveAt(0);
-            List<Point> pList = new List<Point>();
-            pList.Add(point1);
-            pList.Add(point2);
+            HorizontalLine line = new HorizontalLine(5,10,8,'+');
+            line.Draw();
 
             Console.ReadKey();
 
