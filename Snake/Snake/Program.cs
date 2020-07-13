ef5d4b1bcbeadc1a7313a8b618a4a5eb7367c2c5
Ovsyannikov Sergey <Oza1999@mail.ru>
2020-07-13 12:25:57 +0300

    CourseTask10

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -24,10 +24,17 @@ namespace Snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Console.ReadKey();
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo Key = Console.ReadKey();
+                    snake.HandleSnake(Key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
+           
 
         }
     }
--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -8,7 +8,7 @@ namespace Snake
 {
     class Snake:Figure
     {
-        Direction _direction;
+        public Direction _direction;
         public Snake(Point tail, int length, Direction direction)
         {
             _direction = direction;
@@ -38,6 +38,17 @@ namespace Snake
             Point nextPoint = new Point(head);
             nextPoint.Move(1, _direction);
             return nextPoint;
+        }
+        public void HandleSnake(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow)
+                _direction = Direction.LEFT;
+            else if (key == ConsoleKey.RightArrow)
+                _direction = Direction.RIGHT;
+            if (key == ConsoleKey.DownArrow)
+                _direction = Direction.DOWN;
+            else if (key == ConsoleKey.UpArrow)
+                _direction = Direction.UP;
         }
     }
 }
