dd272e5fd9b25f10df8f60dae276635466effd27
Ovsyannikov Sergey <Oza1999@mail.ru>
2020-07-13 10:59:51 +0300

    CourseTask6

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -18,6 +18,23 @@ namespace Snake
 
             point2.Draw();
 
+            List<int> list = new List<int>();
+            list.Add(0);
+            list.Add(1);
+            list.Add(2);
+
+            int x = list[0];
+            int y = list[1];
+            int z = list[2];
+
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+            list.RemoveAt(0);
+            List<Point> pList = new List<Point>();
+            pList.Add(point1);
+            pList.Add(point2);
 
             Console.ReadKey();
 
