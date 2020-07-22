e4ac0931458e50aa73e23f4a5b2ce83003d700c4
Ovsyannikov Sergey <Oza1999@mail.ru>
2020-07-22 22:41:59 +0300

    mes

---
+++ Tanks/TanksGame/Bonus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TanksGame
+{
+    public class Bonus : GameObject
+    {
+        public Bonus(int[,] pos, int[,] size, Bitmap img)
+        {
+            Position = pos;
+            Size = size;
+            Bitmap = new Bitmap(img);
+        }
+    }
+}
