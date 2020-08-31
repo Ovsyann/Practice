4fe33519e058a208b00d834b1d46906fc5a7b1d4
Ovsyannikov Sergey <Oza1999@mail.ru>
2020-08-31 18:01:24 +0300

    AddedGreeting

---
+++ Practice Rules for the Result of Practical Task/Practice Rules for the Result of Practical Task/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NetLab.SergeyOvsyannikov.Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// Вывод в консоль приветственной надписи.
+            /// </summary>
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
