ebadf9fd2848621c99a0a905e8a0f72b820e6729
Ovsyannikov Sergey <Oza1999@mail.ru>
2020-10-31 20:46:23 +0300

    Epa: Vector is complete

---
+++ Practice05/Vector/Vector.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace VectorTask
+{
+    public class Vector : IEquatable<Vector>
+    {
+        private Vector(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public static Vector TryCreate(int x, int y, int z)
+        {
+            if (x == 0 && y == 0 && z == 0)
+            {
+                throw new InvalidOperationException(string.Format("Vector with parameters {0}, {1}, {2} does not exists", x, y, z));
+            }
+
+            return new Vector(x, y, z);
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Z { get; private set; }
+
+        public static Vector operator +(Vector left, Vector right)
+        {
+            if(left==null)
+            {
+                throw new ArgumentNullException(string.Format("Vector {0} is null", left));
+            }
+            if (right == null)
+            {
+                throw new ArgumentNullException(string.Format("Vector {0} is null", right));
+            }
+
+            int x = left.X + right.X;
+            int y = left.Y + right.Y;
+            int z = left.Z + right.Z;
+
+            return new Vector(x, y, z);
+        }
+
+        public static Vector operator -(Vector left, Vector right)
+        {
+            if (left == null)
+            {
+                throw new ArgumentNullException(string.Format("Vector {0} is null", left));
+            }
+            if (right == null)
+            {
+                throw new ArgumentNullException(string.Format("Vector {0} is null", right));
+            }
+
+            int x = left.X - right.X;
+            int y = left.Y - right.Y;
+            int z = left.Z - right.Z;
+
+            return new Vector(x, y, z);
+        }
+
+        public static Vector operator *(Vector left, Vector right)
+        {
+            if (left == null)
+            {
+                throw new ArgumentNullException(string.Format("Vector {0} is null", left));
+            }
+            if (right == null)
+            {
+                throw new ArgumentNullException(string.Format("Vector {0} is null", right));
+            }
+
+            int x = left.X * right.X;
+            int y = left.Y * right.Y;
+            int z = left.Z * right.Z;
+
+            return new Vector(x, y, z);
+        }
+
+        public static Vector CrossProduct(Vector left, Vector right)
+        {
+            if (left == null)
+            {
+                throw new ArgumentNullException(string.Format("Vector {0} is null", left));
+            }
+            if (right == null)
+            {
+                throw new ArgumentNullException(string.Format("Vector {0} is null", right));
+            }
+
+            int x = left.Y * right.Z - left.Z * right.Y;
+            int y = left.Z * right.X - left.X * right.Z;
+            int z = left.X * right.Y - left.Y * right.X;
+
+            return new Vector(x, y, z);
+        }
+
+        public override bool Equals(object other)
+        {
+            return this.Equals(other as Vector);
+        }
+
+        public bool Equals(Vector vector)
+        {
+            if (Object.ReferenceEquals(vector, null))
+            {
+                return false;
+            }
+
+            if (Object.ReferenceEquals(this, vector))
+            {
+                return true;
+            }
+
+            if (this.GetType() != vector.GetType())
+            {
+                return false;
+            }
+
+            return (this.X == vector.X) && (this.Y == vector.Y) && (this.Z == vector.Z);
+        }
+
+        public override int GetHashCode()
+        {
+            return (X * 0x100000) + (Y * 0x1000) + Z;
+        }
+
+        public static bool operator ==(Vector left,Vector right)
+        {
+            if (Object.ReferenceEquals(left, null))
+            {
+                if (Object.ReferenceEquals(right, null))
+                {
+                    return true;
+                }
+            }
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Vector left, Vector right)
+        {
+            return !(left == right);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1},{2})",X,Y,Z);
+        }
+    }
+}
---
+++ Practice05/VectorTests/VectorTests.cs
@@ -0,0 +1,231 @@
+using NUnit.Framework;
+using System;
+using VectorTask;
+
+namespace VectorTests
+{
+    public class VectorTests
+    {
+        [Test]
+        public void TestTryCreate_3and4and5_Created()
+        {
+            const int x = 3;
+            const int y = 4;
+            const int z = 5;
+
+            Vector vector = Vector.TryCreate(x, y, z);
+
+            Assert.AreEqual(x, vector.X);
+            Assert.AreEqual(y, vector.Y);
+            Assert.AreEqual(z, vector.Z);
+        }
+
+        [Test]
+        public void TestTryCreate_3and4and5AreNegative_Created()
+        {
+            const int x = -3;
+            const int y = -4;
+            const int z = -5;
+
+            Vector vector = Vector.TryCreate(x, y, z);
+
+            Assert.AreEqual(x, vector.X);
+            Assert.AreEqual(y, vector.Y);
+            Assert.AreEqual(z, vector.Z);
+        }
+
+        [Test]
+        public void TestTryCreate_AllZeroes_Exception()
+        {
+            const int x = 0;
+            const int y = 0;
+            const int z = 0;
+
+            void Creation()
+            {
+                Vector vector = Vector.TryCreate(x, y, z);
+            }
+
+            Assert.Throws<InvalidOperationException>(Creation);
+        }
+
+        [Test]
+        public void TestGetHashCode_3and4and5_Calculated()
+        {
+            const int x = 3;
+            const int y = 4;
+            const int z = 5;
+            Vector vector = Vector.TryCreate(x, y, z);
+            int expected = 3162117;
+
+            int actual = vector.GetHashCode();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestEquals_TwoSameVectors_true()
+        {
+            const int x = 3;
+            const int y = 4;
+            const int z = 5;
+            Vector vector1 = Vector.TryCreate(x, y, z);
+            Vector vector2 = Vector.TryCreate(x, y, z);
+
+            Assert.IsTrue(vector1.Equals(vector2));
+        }
+
+        [Test]
+        public void TestEquals_TwoDifferentVectors_false()
+        {
+            const int x1 = 3;
+            const int y1 = 4;
+            const int z1 = 5;
+            const int x2 = 4;
+            const int y2 = 5;
+            const int z2 = 6;
+            Vector vector1 = Vector.TryCreate(x1, y1, z1);
+            Vector vector2 = Vector.TryCreate(x2, y2, z2);
+
+            Assert.IsFalse(vector1.Equals(vector2));
+        }
+
+
+        [Test]
+        public void TestEquals_OneVectorIsNull_false()
+        {
+            const int x1 = 3;
+            const int y1 = 4;
+            const int z1 = 5;
+            Vector vector1 = Vector.TryCreate(x1, y1, z1);
+            Vector vector2 = null;
+
+            Assert.IsFalse(vector1.Equals(vector2));
+        }
+
+
+        [Test]
+        public void TestOverridenEquality_TwoSameVectors_true()
+        {
+            const int x1 = 3;
+            const int y1 = 4;
+            const int z1 = 5;
+            Vector vector1 = Vector.TryCreate(x1, y1, z1);
+            Vector vector2 = Vector.TryCreate(x1, y1, z1);
+
+            Assert.IsTrue(vector1 == vector2);
+        }
+
+        [Test]
+        public void TestOverridenEquality_TwoDifferentVectors_False()
+        {
+            const int x1 = 3;
+            const int y1 = 4;
+            const int z1 = 5;
+            const int x2 = 4;
+            const int y2 = 5;
+            const int z2 = 6;
+            Vector vector1 = Vector.TryCreate(x1, y1, z1);
+            Vector vector2 = Vector.TryCreate(x2, y2, z2);
+
+            Assert.IsFalse(vector1 == vector2);
+        }
+
+        [Test]
+        public void TestOverridenEquality_OneVectorIsNull_False()
+        {
+            const int x1 = 3;
+            const int y1 = 4;
+            const int z1 = 5;
+            Vector vector1 = Vector.TryCreate(x1, y1, z1);
+            Vector vector2 = null;
+
+            Assert.IsFalse(vector1 == vector2);
+        }
+
+        [Test]
+        public void TestOverridenEquality_BothVectorsAreNull_True()
+        {
+            Vector vector1 = null;
+            Vector vector2 = null;
+
+            Assert.IsTrue(vector1 == vector2);
+        }
+
+
+        [Test]
+        public void TestOverridenNonEquality_TwoSameVectors_False()
+        {
+            const int x1 = 3;
+            const int y1 = 4;
+            const int z1 = 5;
+            Vector vector1 = Vector.TryCreate(x1, y1, z1);
+            Vector vector2 = Vector.TryCreate(x1, y1, z1);
+
+            Assert.IsFalse(vector1 != vector2);
+        }
+
+        [Test]
+        public void TestOverridenNonEquality_TwoDifferentVectors_True()
+        {
+            const int x1 = 3;
+            const int y1 = 4;
+            const int z1 = 5;
+            const int x2 = 4;
+            const int y2 = 5;
+            const int z2 = 6;
+            Vector vector1 = Vector.TryCreate(x1, y1, z1);
+            Vector vector2 = Vector.TryCreate(x2, y2, z2);
+
+            Assert.IsTrue(vector1 != vector2);
+        }
+
+        [Test]
+        public void TestOverridenNonEquality_OneVectorIsNull_True()
+        {
+            const int x1 = 3;
+            const int y1 = 4;
+            const int z1 = 5;
+            Vector vector1 = Vector.TryCreate(x1, y1, z1);
+            Vector vector2 = null;
+
+            Assert.IsTrue(vector1 != vector2);
+        }
+
+        [Test]
+        public void TestOverridenNonEquality_BothVectorsAreNull_False()
+        {
+            Vector vector1 = null;
+            Vector vector2 = null;
+
+            Assert.IsFalse(vector1 != vector2);
+        }
+
+        [Test]
+        public void TestToString_TrueVector_Success()
+        {
+            const int x = 3;
+            const int y = 4;
+            const int z = 5;
+            Vector vector = Vector.TryCreate(x, y, z);
+            string expected = "(3,4,5)";
+
+            string actual = vector.ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestToString_NullVector_Exception()
+        {
+            Vector vector = null;
+
+            void Toline()
+            {
+                string actual = vector.ToString();
+            }
+
+            Assert.Throws<NullReferenceException>(Toline);
+        }
+    }
+}
