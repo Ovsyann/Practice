c57942bd08e3db6ab068872c4559d110de612fb7
Ovsyann <Oza1999@mail.ru>
2020-11-11 17:38:59 +0300

    Csharp7Begining

---
+++ CSharp_07/Matrix/Matrix.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Matrix
+{
+    public class Matrix : IEquatable<Matrix>
+    {
+        private readonly int[,] elements;
+
+        public Matrix(int rowsCount, int columnsCount)
+        {
+            elements = new int[rowsCount, columnsCount];
+        }
+
+        public Matrix(int[,] matrix)
+        {
+            //!!!
+        }
+
+        public override bool Equals(object other)
+        {
+            return this.Equals(other as Matrix);
+        }
+
+        public bool Equals(Matrix other)
+        {
+            if (Object.ReferenceEquals(other, null))
+            {
+                return false;
+            }
+
+            if (Object.ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            if (this.GetType() != other.GetType())
+            {
+                return false;
+            }
+
+            //реализация сравнения
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
