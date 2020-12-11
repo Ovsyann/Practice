2b30173e10896f645a2099fc5007d35d7c0f9b68
Argonaut <Oza1999@mail.ru>
2020-12-11 21:00:07 +0300

    ISReliabilityLab6
    BeginUI

---
+++ SystemSimulation/SystemSimulationUI/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace SystemSimulationUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series15 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartElementsLife = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.labelAverageResurrectTime = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chartElementsLife)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartElementsLife
+            // 
+            this.chartElementsLife.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            chartArea3.Name = "ChartArea1";
+            this.chartElementsLife.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chartElementsLife.Legends.Add(legend3);
+            this.chartElementsLife.Location = new System.Drawing.Point(9, 9);
+            this.chartElementsLife.Margin = new System.Windows.Forms.Padding(0);
+            this.chartElementsLife.Name = "chartElementsLife";
+            this.chartElementsLife.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series11.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            series11.Legend = "Legend1";
+            series11.LegendText = "Элемент1";
+            series11.Name = "Series1";
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series12.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            series12.Legend = "Legend1";
+            series12.LegendText = "Элемент2";
+            series12.Name = "Series2";
+            series13.ChartArea = "ChartArea1";
+            series13.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series13.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            series13.Legend = "Legend1";
+            series13.LegendText = "Элемент3";
+            series13.Name = "Series3";
+            series14.ChartArea = "ChartArea1";
+            series14.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series14.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            series14.Legend = "Legend1";
+            series14.LegendText = "Элемент4";
+            series14.Name = "Series4";
+            series15.ChartArea = "ChartArea1";
+            series15.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series15.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            series15.Legend = "Legend1";
+            series15.LegendText = "Элемент5";
+            series15.Name = "Series5";
+            this.chartElementsLife.Series.Add(series11);
+            this.chartElementsLife.Series.Add(series12);
+            this.chartElementsLife.Series.Add(series13);
+            this.chartElementsLife.Series.Add(series14);
+            this.chartElementsLife.Series.Add(series15);
+            this.chartElementsLife.Size = new System.Drawing.Size(668, 426);
+            this.chartElementsLife.TabIndex = 0;
+            this.chartElementsLife.Text = "chart1";
+            // 
+            // labelAverageResurrectTime
+            // 
+            this.labelAverageResurrectTime.AutoSize = true;
+            this.labelAverageResurrectTime.Location = new System.Drawing.Point(686, 114);
+            this.labelAverageResurrectTime.Name = "labelAverageResurrectTime";
+            this.labelAverageResurrectTime.Size = new System.Drawing.Size(171, 13);
+            this.labelAverageResurrectTime.TabIndex = 1;
+            this.labelAverageResurrectTime.Text = "Среднее время восстановления";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(689, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(168, 37);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(689, 55);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(168, 37);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 450);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.labelAverageResurrectTime);
+            this.Controls.Add(this.chartElementsLife);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartElementsLife)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartElementsLife;
+        private System.Windows.Forms.Label labelAverageResurrectTime;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ SystemSimulation/SystemSimulationUI/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemSimulationUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateChart(/*аргументы*/);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Interval = 500;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void UpdateChart(/*параметры*/)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
