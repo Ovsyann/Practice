4b7df632694ff1cd024cbcd4146bb192d476a747
Ovsyann <Oza1999@mail.ru>
2020-12-16 13:33:38 +0300

    AddedLab9

---
+++ OneDimWalkLab9/OneDimRandomWalk/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace OneDimRandomWalk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart2
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea1);
+            this.chart2.Location = new System.Drawing.Point(16, 337);
+            this.chart2.Margin = new System.Windows.Forms.Padding(4);
+            this.chart2.Name = "chart2";
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Distribution";
+            this.chart2.Series.Add(series1);
+            this.chart2.Size = new System.Drawing.Size(866, 265);
+            this.chart2.TabIndex = 13;
+            this.chart2.Text = "chart5";
+            title1.Name = "Title1";
+            title1.Text = "Статистическая функция распределения";
+            this.chart2.Titles.Add(title1);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(16, 15);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Name = "Frequencies";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(866, 277);
+            this.chart1.TabIndex = 12;
+            this.chart1.Text = "chart6";
+            title2.Name = "Title1";
+            title2.Text = "Гистограмма частот";
+            this.chart1.Titles.Add(title2);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(405, 295);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 295);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(905, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Mx = ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(905, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "dx = ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(905, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "N = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1181, 617);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ OneDimWalkLab9/OneDimRandomWalk/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OneDimRandomWalk
+{
+    public partial class Form1 : Form
+    {
+        double leftXBorder;
+        double rightXBorder;
+        double intervalsCount = 80;
+        int ExperimentsCount = 500;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double[] randomValues = WalkModeling.Calculate();
+            leftXBorder = randomValues.Min();
+            rightXBorder = randomValues.Max();
+            double averageDistance = randomValues.Sum() / randomValues.Length;
+
+            double mx = StatisticData.CalculateMathExpectation(randomValues);
+            double dx = StatisticData.CalculateDispersoin(randomValues, mx);
+            double N = 1.96 * dx / 0.001;
+            label3.Text = "Mx = " + mx;
+            label4.Text = "dx = " + dx;
+            label5.Text = "N = " + N;
+            // GetAverageDistances();
+
+            ConstructFrequencyHistogram(randomValues);
+            ConstructStatisticalDistributionFunction(randomValues);
+
+            //вычисление критерия Колмагорова
+            double kolmogorovCriterionLambdaValue = KolmogorovCriterion.GetLambda(randomValues);
+            double kolmogorovCriterionValue = KolmogorovCriterion.Calculate(randomValues, randomValues.Length);
+            label1.Text = "средняя дистанция = " + averageDistance;
+            label2.Text = $"Лямбда по критерию Колмогорова равна {Math.Round(kolmogorovCriterionLambdaValue, 3)}; \nОценка генератора " +
+                $"случайных чисел равна {kolmogorovCriterionValue}";
+        }
+
+        //private void GetAverageDistances()
+        //{
+        //    double[] averageDistances = new double[ExperimentsCount];
+        //    for(int i = 0; i < ExperimentsCount; i++)
+        //    {
+        //        double[] randomVariables = WalkModeling.Calculate();
+        //        averageDistances[i] = randomVariables.Sum() / randomVariables.Length;
+        //    }
+
+        //    double expectation = ShowMathExpectation(averageDistances);
+        //    double squaredDispersion = ShowSquaredDispersion(averageDistances, expectation);
+        //    double N = 1.96 * squaredDispersion / 0.000001;
+        //    label3.Text = "Mx = " + expectation;
+        //    label4.Text = "dx = " + squaredDispersion;
+        //    label5.Text = "N = " + N;
+        //}
+
+        private double ShowSquaredDispersion(double[] averageDistances, double expectation)
+        {
+            double dispersion = StatisticData.CalculateDispersoin(averageDistances, expectation);
+            double squareDispersion = dispersion / Math.Sqrt(ExperimentsCount);
+            return squareDispersion;
+        }
+
+        private double ShowMathExpectation(double[] averageDistances)
+        {
+            double mx = StatisticData.CalculateMathExpectation(averageDistances);
+            return mx;
+        }
+
+        List<int> DrawingPreparation(double[] randomVariables)
+        {
+            double histogramStep = (double)randomVariables.Max() / intervalsCount;
+            List<int> listOfRepetitions = new List<int>((int)intervalsCount);
+
+            for (int i = 0; i < intervalsCount; i++)
+            {
+                listOfRepetitions.Add(randomVariables.Count(p => p >= i * histogramStep && p < (i + 1) * histogramStep));
+            }
+
+            return listOfRepetitions;
+        }
+
+        void ConstructFrequencyHistogram(double[] randomValues)
+        {
+            chart1.ChartAreas[0].AxisX.Minimum = leftXBorder;
+            chart1.ChartAreas[0].AxisX.Maximum = rightXBorder;
+            List<int> listOfRepetitions = DrawingPreparation(randomValues);
+            double[] scaledRepetitions = new double[listOfRepetitions.Count];
+
+            double step = randomValues.Max() / intervalsCount;
+            double intervalValue = 0;
+
+            for (int i = 0; i < listOfRepetitions.Count; i++)
+            {
+                scaledRepetitions[i] = (double)listOfRepetitions[i] / randomValues.Length / 10 * intervalsCount;
+            }
+
+            for (int i = 0; i < scaledRepetitions.Length; i++)
+            {
+                double asix = intervalValue + step / 2;
+                chart1.Series[0].Points.AddXY(asix, scaledRepetitions[i]);
+                intervalValue += step;
+            }
+        }
+
+        private void ConstructStatisticalDistributionFunction(double[] randomValues)
+        {
+            chart2.ChartAreas[0].AxisX.Minimum = leftXBorder;
+            chart2.ChartAreas[0].AxisX.Maximum = rightXBorder;
+            List<int> listOfRepetitions = DrawingPreparation(randomValues);
+            double[] scaledRepetitions = new double[listOfRepetitions.Count];
+
+            double step = randomValues.Max() / intervalsCount;
+            double intervalValue = 0;
+
+            for (int i = 0; i < listOfRepetitions.Count; i++)
+            {
+                scaledRepetitions[i] = (double)listOfRepetitions[i] / randomValues.Length;
+            }
+
+            for (int i = 0; i < scaledRepetitions.Length; i++)
+            {
+                double asix = intervalValue + step / 2;
+
+                chart2.Series[0].Points.AddXY(asix, scaledRepetitions.Take(i + 1).Sum());
+                intervalValue += step;
+            }
+        }
+    }
+}
